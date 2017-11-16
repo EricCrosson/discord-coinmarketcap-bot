ad6bced34f024643ee7a376a2a9cd63fc445a9a5
Eric Crosson <eric.s.crosson@gmail.com>
2017-11-16 00:04:24 -0600

    Add embedded images of chart history
    from coinmarketcap.com

--- index.js
+++ index.js
@@ -6,10 +6,6 @@
 
 const _ = require('lodash');
 const coinmarketcap = require('coinmarketcap-cli-api');
-const stripAnsi = require('strip-ansi');
-const Table = require('markdown-table');
-const CliTable = require('cli-table2');
-const regexp_quote = require('regexp-quote');
 const imgur = require('imgur');
 const webshot = require('webshot');
 
@@ -18,6 +14,8 @@ const client = new Discord.Client();
 const config = require('./config.json');
 
 const crypto_mkcap_dominance_png = 'crypto-mkcap-dominance.png';
+
+// TODO: refactor the photo-fetching into the coinmarketcap-api library
 
 
 client.on('ready', () => {
@@ -43,13 +41,13 @@ client.on('message', message => {
 
         var market = '';
         var marketcap = '';
+        let imageUrl = '';
         var totalmarketcap = 0;
 
         let match = message.content.match(new RegExp(/^.(\S+)\s*(.*)$/), '');
         console.log(match);
         if (match[2] === '') {
 
-            let imageUrl = '';
 
             // no arguments provided: list top 10 market caps
             promises.push(coinmarketcap.getMarketCaps()
@@ -64,32 +62,17 @@ client.on('message', message => {
 
             promises.push(new Promise(function(resolve, reject) {
                 let options = {
-                    shotSize: {
-                        width: 1000,
-                        height: 752
-                    },
-                    shotOffset: {
-                        left: 25,
-                        right: 0,
-                        top: 1650,
-                        bottom: 0
-                    }
+                    shotSize: {width: 1000, height: 752},
+                    shotOffset: {left: 25, right: 0, top: 1650, bottom: 0}
                 };
                 webshot('https://coinmarketcap.com/charts/', crypto_mkcap_dominance_png, options, function(err) {
-                    if (err) {
-                        console.log('Error detected')
-                        console.log('Removing marketcap-dominance chart')
-                        reject();
-                    }
+                    if (err) { reject() }
                     // upload image to imgur
                     imgur.uploadFile(crypto_mkcap_dominance_png)
                         .then(function(json) {
-                            console.log(`Imgur link: ${json.data.link}`)
                             imageUrl = json.data.link;
                             resolve();
-                        }).catch(function(err) {
-                            console.log(err);
-                        });
+                        }).catch(function(err) { console.log(err) });
                 })
             }))
 
@@ -120,30 +103,53 @@ client.on('message', message => {
             promises.push(coinmarketcap.getMarketCap(coin)
                           .then(function(marketcaps) {marketcap = marketcaps[0];}));
 
+
             // TODO: add error handling (coin not found)
             // TODO: host on a forever home
             Promise.all(promises)
                 .then(function() {
 
-                    let marketData = _.reduce(market, function(sum, data) {
-                        const marketShare = parseFloat(data['Volume (%)'].replace('%',''));
-                        // Limit this list to 10 entries
-                        if (sum.length >= 10) return sum;
-                        // Don't display dust entries (small market share)
-                        if (marketShare < 3) return sum;
+                    let options = {
+                        shotSize: {width: 1000, height: 662},
+                        shotOffset: {left: 12, right: 0, top: 742, bottom: 0}
+                    };
+                    let photoURL = `https://coinmarketcap.com/currencies/${marketcap['Currency'].toLowerCase().replace(/\s+/,'-')}`
+                    webshot(photoURL,
+                            crypto_mkcap_dominance_png, options, function(err) {
+                                if (err) { throw err }
+                                // upload image to imgur
+                                imgur.uploadFile(crypto_mkcap_dominance_png)
+                                    .then(function(json) {
+                                        imageUrl = json.data.link;
 
-                        console.log(data);
-                        console.log(sum);
-                        sum.push({name: `${data['Exchange']}`,
-                                  value: `Pair: ${data['Pair']}\nVolume (%): ${data['Volume (%)']}\nPrice: ${data['Price']}`});
-                        return sum;
-                    }, []);
-                    message.channel.send({embed: {
-                        title: `${marketcap['Currency']} market leaders by volume`,
-                        description: `Total market cap: ${marketcap['Market cap']}\nMarket cap rank: ${marketcap['Market cap rank']}`,
-                        url: `https://coinmarketcap.com/currencies/${marketcap['Currency'].toLowerCase().replace(/\s+/,'-')}/#markets`,
-                        fields: marketData
-                    }});
+                                        let embed = new Discord.RichEmbed()
+                                            .setTitle(`${marketcap['Currency']} market leaders by volume`)
+                                            .setDescription(`Total market cap: ${marketcap['Market cap']}\nMarket cap rank: ${marketcap['Market cap rank']}`)
+                                            .setURL(`https://coinmarketcap.com/currencies/${marketcap['Currency'].toLowerCase().replace(/\s+/,'-')}/#markets`)
+
+                                        /// main-y
+                                        _.reduce(market, function(sum, data) {
+                                            const marketShare = parseFloat(data['Volume (%)'].replace('%',''));
+                                            // Limit this list to 10 entries
+                                            if (sum.length >= 10) return sum;
+                                            // Don't display dust entries (small market share)
+                                            if (marketShare < 3) return sum;
+
+                                            console.log(data);
+                                            console.log(sum);
+                                            embed.addField(`${data['Exchange']}`,
+                                                           `Pair: ${data['Pair']}\nVolume (%): ${data['Volume (%)']}\nPrice: ${data['Price']}`);
+                                            return sum;
+                                        }, []);
+
+                                        embed.setImage(imageUrl)
+                                        message.channel.send({embed});
+                                        /// main-y end
+
+
+
+                                    }).catch(function(err) { throw err });
+                            })
                 })
         }
 
